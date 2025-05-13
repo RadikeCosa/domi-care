438b4f870a3d84da2f288445fa81456e20917c56
Ramiro Cosa <ramirocosa@hotmail.com>
2025-05-13 13:46:52 -0300

    layout fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px]  min-h-screen  gap-16 sm:p-20 ">
       <h1 className="text-4xl font-bold text-center">DomiCare</h1>
-      <p className="">Una app para equipos de atencion domiciliaria</p>
+      <main>
+        <p className="text-center">
+          Una app para equipos de atencion domiciliaria
+        </p>
+        <Link className="text-center" href="/dashboard">
+          {" "}
+          ir a Dashboard
+        </Link>
+      </main>
+      <footer className="text-sm text-center">© 2025 DomiCare</footer>
     </div>
   );
 }
--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -1,5 +1,7 @@
 export default function SideNav() {
   return (
-    <div className="flex h-screen flex-col bg-gray-800 text-white">SideNav</div>
+    <div className="flex h-screen flex-col bg-blue-100 ">
+      <h3 className="text-center m-3 text-3xl">Side Nav</h3>
+    </div>
   );
 }
